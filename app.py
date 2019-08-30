d1a79eee551fc43a9d8cc59f2ad750f8ec01f49f
dred <vn.joner@gmail.com>
2019-08-30 18:02:43 +0700

    update login_logout basic

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request, session
 
+app = Flask(__name__)
+app.secret_key = "super secret key"
 
 @app.route("/Home")
 def Home():
@@ -14,6 +15,28 @@ def SignUp():
 def SignIn():
 	return render_template('DangNhap.html')
 
+@app.route("/login", methods=['POST'])
+def login():
+	_ID = request.form['id']
+	_password = request.form['password']
+	
+	if _ID == 'admin'and _password == 'admin' :
+		session['id'] = _ID
+		return 'Success!!!'
+	else:
+		return 'Faile!!!'
+
+@app.route("/logout", methods=["POST"])
+def logout():
+	if 'id' in session:
+		session.pop('id',None)
+		return 'logout Success'
+	else: 
+		return 'do not login'
+
 @app.route("/")
-def main():
+def welcome():
 	return Home()
+
+if __name__ == '__main__':
+	app.run(host='localhost',port=5000,debug=True)
